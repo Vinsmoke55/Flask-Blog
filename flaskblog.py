c0c393ecc2627160160a67cd748b727ecd70ef3b
VINANK <neupaneayush3@gmail.com>
2023-07-29 19:17:58 +0545

    form and user input

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash,redirect
+from form import RegistrationForm,LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='4e46bf8c58b1c5be82332c606adf2d4d'
 
 posts=[
 {
@@ -25,6 +28,25 @@ def home():
 @app.route("/about")
 def about():
 	return render_template('about.html',title='About')
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+	form=RegistrationForm()
+	if form.validate_on_submit():
+		flash(f"Account have been created for {form.username.data}",'success')
+		return redirect(url_for('home'))
+	return render_template('register.html',title='register',form=form)
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+	form=LoginForm()
+	if form.validate_on_submit():
+		if form.email.data=="neupaneayush3@gmail.com" and form.password.data=="password":
+			flash("You are logged in ",'success')
+			return redirect(url_for('home'))
+		else:
+			flash("login failed",'danger')
+	return render_template('login.html',title='login',form=form)
 
 if __name__=='__main__':
 	app.run(debug=True)
